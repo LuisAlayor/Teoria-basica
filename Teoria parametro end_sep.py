a18a7584121a4dd1ad67c7a88d30b68763ebc4ca
Luis Enrique Alayo Rojas <74937402+LuisAlayor@users.noreply.github.com>
2021-01-25 23:51:55 +0100

    Add files via upload

---
+++ Teoria parametro end_sep.py
@@ -0,0 +1,14 @@
+print("================")
+print("Parametro end")
+print("================\n")
+
+print("Estos es un", end=" ")
+print("ejemplo")
+
+print("\n\n================")
+print("Parametro sep")
+print("================\n")
+
+print("1","2","3","4","5", sep="=")
+
+print("\nFin.")
