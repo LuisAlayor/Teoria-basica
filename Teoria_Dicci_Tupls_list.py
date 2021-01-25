25985e0b3138f3a1e130a5cda031a92a87f3113c
Luis Enrique Alayo Rojas <74937402+LuisAlayor@users.noreply.github.com>
2021-01-25 23:52:04 +0100

    Add files via upload

---
+++ Teoria_Dicci_Tupls_list.py
@@ -0,0 +1,100 @@
+#print("========")
+#print("Listas")
+#print("========\n")
+
+#demo_list = [1, "hello", 1.34, [1, 2, 3]]
+#numbers_list = list((1, 2, 3, 4))
+
+#print(demo_list)
+#print(numbers_list)
+#print(type(numbers_list))
+
+
+
+#print("\n========")
+#print("Lista con rangos")
+#print("========\n")
+
+#prueba_rango = list(range(1, 11))
+#print(prueba_rango)
+
+
+
+#print("\n========")
+#print("Pruebas con listas")
+#print("========\n")
+
+#colors = ['green', 'blue', 'red']
+#colors += ['yellow']
+#print(colors)
+#print('green' in colors)
+#colors[0] = 'black'
+#print(colors)
+#colors.append('violet')
+#colors.extend(['violet', 'white'])
+#colors.insert(-1, 'violet')
+#colors.insert(len(colors), 'violet')
+#print(colors)
+#colors.remove('red')
+#colors.pop(1)
+#colors.clear()
+#colors.sort(reverse=True)
+#print(colors.index('blue'))
+
+#print("\n========")
+#print("Tuples")
+#print("========\n")
+
+#x = (1, 2, 3, 4, 5, 6)
+#y = tuple((1, 2, 3))
+#z = (9,)
+#print(dir(tuple))
+#print(x[5])
+#NO PUEDES AÑADIR MAS VALORES COMO CON LAS LISTAS
+#location = {
+    #(35.125, 39.365):"Tokio",
+    #(20.125, 150.365):"Nueva York"
+    #}
+#print(location)
+
+
+
+#print("\n========")
+#print("SET")
+#print("========\n")
+
+#colors = {'green', 'blue', 'red'}
+#colors.add('white')
+#colors.remove('red')
+#colors.clear()
+#del colors
+#print(colors)
+
+#print("\n========")
+#print("Diccionario")
+#print("========\n")
+
+#product = {
+    #"name":"book",
+    #"quantity" :"3",
+    #"price": 5.99
+    #}
+#print(product)
+
+person = {
+    'nombre': 'Luis',
+    'apellido': 'Alayo',
+    'edad': 30
+    }
+#print(person.items())
+#del person
+#person.clear()
+print(person)
+
+
+
+
+
+
+
+
