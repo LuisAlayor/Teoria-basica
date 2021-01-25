ad6f70a2ed74bf39fccb95eeb3c3415bb0d4d7b4
Luis Enrique Alayo Rojas <74937402+LuisAlayor@users.noreply.github.com>
2021-01-25 23:52:51 +0100

    Add files via upload

---
+++ Teoria_Ciclo o bucles.py
@@ -0,0 +1,49 @@
+print("==============")
+print("While y For")
+print("==============\n")
+
+
+
+#foods = ["apples", 'bread', 'cheese', 'milk', 'bananas','graves']
+#recorre cada una de los alimentos en la lista
+#for food in foods:
+    #if food == 'cheese':
+        #print("tienes que comprar esto")
+        #break
+        #continue
+    #print(food)
+
+
+
+##for letter in 'hello':
+##    print(letter)
+
+
+
+
+
+##x = 1
+##while x < 3:
+##    print(x)
+##    x += 1   
+
+
+
+##x = 1
+##while x < 11:
+##    print("Ernesto")
+##    x += 1   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
