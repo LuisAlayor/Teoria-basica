67fd62cbdf41924c7cf215dfbf704301f09ed9da
Luis Enrique Alayo Rojas <74937402+LuisAlayor@users.noreply.github.com>
2021-01-29 21:32:41 +0100

    Add files via upload

---
+++ _Teoria_primera operacion de dos variables.py
@@ -0,0 +1,10 @@
+nombre = input("Te llamas?:  ")
+print("Que tal" + nombre + ", vamos a realizar una operación.")
+
+num_uno = int(input("introduce el primer num de tu tfno:  "))
+num_dos = int(input("introduce desde el segundo hasta el cuarto num de tu tfno"))
+
+resultado = num_uno + num_dos
+
+print(nombre + " El resultado de la operacion es:  ", resultado)
+
